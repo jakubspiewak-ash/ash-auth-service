b27ddb219b4b43da4bacb5b98502deb8d365d943
Jakub Śpiewak <priv@jakubspiewak.com>
2021-09-29 15:33:26 +0200

    controller refactor

--- src/main/java/com/jakubspiewak/ashauthservice/service/AuthController.java
+++ src/main/java/com/jakubspiewak/ashauthservice/service/AuthController.java
@@ -3,11 +3,13 @@ package com.jakubspiewak.ashauthservice.service;
 import com.jakubspiewak.ashapimodellib.model.auth.ApiTokenInfo;
 import com.jakubspiewak.ashapimodellib.model.auth.ApiUserCredentials;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import static org.springframework.http.HttpStatus.OK;
 
+@Slf4j
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
@@ -18,14 +20,12 @@ public class AuthController {
   @PostMapping
   ResponseEntity<String> createToken(@RequestBody ApiUserCredentials credentials) {
     final var token = authService.createToken(credentials);
-
     return ResponseEntity.status(OK).body(token);
   }
 
   @GetMapping("/{token}")
   ResponseEntity<ApiTokenInfo> resolveToken(@PathVariable String token) {
-    final var id = authService.resolveToken(token);
-
-    return ResponseEntity.status(OK).body(id);
+    final var tokenInfo = authService.resolveToken(token);
+    return ResponseEntity.status(OK).body(tokenInfo);
   }
 }
